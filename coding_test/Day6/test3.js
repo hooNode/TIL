222309c3a5b0ffe493569b34fb8630d27ab75137
kth7024 <guinness5167@gmail.com>
2022-01-19 10:14:25 +0900

    Day7

--- coding_test/Day6/test3.js
+++ coding_test/Day6/test3.js
@@ -1,7 +1,6 @@
-//단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
-
-function solution(s) {
-    let newArr = s.split("")
-    if (s.length % 2 === 1) return newArr[Math.floor(s.length / 2)]
-    return newArr.slice(Math.floor(s.length / 2) - 1, Math.round(s.length / 2) + 1).join("")
+//String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아,
+//"김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요.
+//seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+function solution(seoul) {
+    return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
 }
