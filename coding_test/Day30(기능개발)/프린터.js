fa9343ce5be05775ac3835d3720fa6bdb7e627ec
kth7024 <guinness5167@gmail.com>
2022-03-12 00:12:46 +0900

    프린터

---
+++ coding_test/Day30(기능개발)/프린터.js
@@ -0,0 +1,20 @@
+function solution(priorities, location) {
+  var arr = priorities.map((priority, index) => ({
+    index: index,
+    priority: priority,
+  }));
+
+  var queue = [];
+
+  while (arr.length > 0) {
+    var firstEle = arr.shift();
+    var hasHighPriority = arr.some((ele) => ele.priority > firstEle.priority);
+    if (hasHighPriority) {
+      arr.push(firstEle);
+    } else {
+      queue.push(firstEle);
+    }
+  }
+
+  return queue.findIndex((queueEle) => queueEle.index === location) + 1;
+}
